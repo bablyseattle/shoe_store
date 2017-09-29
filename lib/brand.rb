6d3864a5e237741650b6be446505f42ac26de856
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-09-29 16:45:37 -0700

    add style to all the pages

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ActiveRecord::Base
 	has_and_belongs_to_many(:stores)
 	
-validates(:name, {:presence => true, :uniqueness => true, :length => { :maximum => 100 }})
+validates(:name, {:presence => true, uniqueness: { case_sensitive: false }, :length => { :maximum => 100 }})
   	before_save(:capitalizes_name, {})
 
   	private
@@ -10,4 +10,7 @@ validates(:name, {:presence => true, :uniqueness => true, :length => { :maximum 
 		    name = self.name.split(" ")
 		    self.name = name.map{ |word| word.upcase }.join(" ")
 		end
+
+		private
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
 	has_and_belongs_to_many(:brands)
 
-validates(:name, {:presence => true, :uniqueness => true, :length => { :maximum => 100 }})  	
+validates(:name, {:presence => true, uniqueness: { case_sensitive: false }, :length => { :maximum => 100 }})  	
 before_save(:capitalizes_name, {})
 
   	private
