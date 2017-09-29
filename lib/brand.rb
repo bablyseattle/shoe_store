1d0a73a76edda57a07298ec718cccbed103c9e72
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-09-29 14:52:31 -0700

    add validations to brand and store class

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,13 @@
 class Brand < ActiveRecord::Base
 	has_and_belongs_to_many(:stores)
+	
+validates(:name, {:presence => true, :uniqueness => true, :length => { :maximum => 100 }})
+  	before_save(:capitalizes_name, {})
+
+  	private
+
+		  def capitalizes_name
+		    name = self.name.split(" ")
+		    self.name = name.map{ |word| word.upcase }.join(" ")
+		end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
 	has_and_belongs_to_many(:brands)
+
+validates(:name, {:presence => true, :uniqueness => true, :length => { :maximum => 100 }})  	
+before_save(:capitalizes_name, {})
+
+  	private
+
+		  def capitalizes_name
+		    name = self.name.split(" ")
+		    self.name = name.map{ |word| word.upcase }.join(" ")
+		end
+
 end
